6ec9187d32a3190e9705b385245d518efef3499c
kinjal5104 <144837128+kinjal5104@users.noreply.github.com>
2024-10-15 13:48:48 +0530

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart'; // Firebase import
-import 'screens/login_screen.dart';
-import 'screens/registration_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'screens/login_screen.dart'; // Updated import for login screen
+import 'screens/registration_screen.dart'; // Updated import for registration screen
 import 'screens/shop_details_screen.dart';
 import 'screens/add_product_screen.dart';
 import 'screens/product_success_screen.dart';
@@ -22,19 +23,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'ShopLocalia',
-        theme: ThemeData(
-          primarySwatch: Colors.green,
-        ),
-        initialRoute: '/',
-        routes: {
-    '/': (context) => LoginScreen(),
-    '/registration': (context) => RegistrationScreen(),
-    '/shop-details': (context) => ShopDetailsScreen(),
-    '/add-product': (context) => AddProductScreen(products: [], shopId: ''), // Ensure shopId is passed
-    '/successScreen': (context) => ProductSuccessScreen(),  // Frame 53
-    // Add more routes as needed
-    },
-        );
-    }
+      title: 'ShopLocalia',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginScreen(),
+        '/registration': (context) => RegistrationScreen(),
+        '/shop-details': (context) => ShopDetailsScreen(),
+        '/add-product': (context) => AddProductScreen(products: [], shopId: ''), // Ensure shopId is passed
+        '/successScreen': (context) => ProductSuccessScreen(),  // Frame 53
+        // Add more routes as needed
+      },
+    );
+  }
 }
